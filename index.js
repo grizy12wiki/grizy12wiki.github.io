33f54a895893740b1f01e6c5bb21347d614d4fec
grizy12wiki <sillylittleantifaorca2@proton.me>
2024-11-22 23:54:17 +0100

    liste fonctionnelle

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
+document.addEventListener( 'click', mouseEventsListener);
+
+
+//to check if list elements have been clicked
+function mouseEventsListener ( event ) {
+
+
+    switch( event.target.id ) {
+
+        case 'A1':
+            load_html_into("main","articles/main.html");
+            break;
+        case 'A2':
+            load_html_into("main","articles/slip.html");
+            break;
+        case 'A3':
+            load_html_into("main","articles/homosexuality.html");
+            break;
+        case 'A4':
+            load_html_into("main","articles/pogo.html");
+            break
+        case 'A5':
+            load_html_into("main","articles/chat.html");
+            break;
+        default:
+            break;
+
+    }
+
+}
+
 function load_html_into(id,path) {
     document.getElementById(id).innerHTML='<object type="text/html" data="' + path + '" ></object>';
 }
 
 function main(){
-    load_html_into("liste","articles.html");
     load_html_into("header","header.html");
-    load_html_into("main","articles/slip.html");
-    setTimeout(function(){
-                load_html_into("main","articles/chat.html");
-    }, 2000);
+    load_html_into("main","articles/main.html");
 
 }
